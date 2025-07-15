9ca7d8930698946ef7dacf661f515478351e1a4c
Rohit Bhardwaj <rohitkrbhardwaj09@gmail.com>
2025-07-15 22:58:57 +0530

    video 131

--- src/test/java/section_016/Day1.java
+++ src/test/java/section_016/Day1.java
@@ -5,8 +5,13 @@ import org.testng.annotations.Test;
 public class Day1 {
 
 	@Test
-	public void demo() {
+	public void demoTest1() {
 		System.out.println("Hello");
+	}
+	
+	@Test
+	public void demoTest2() {
+		System.out.println("Bye");
 	}
 	
 }
---
+++ src/test/java/section_016/Day2.java
@@ -0,0 +1,12 @@
+package section_016;
+
+import org.testng.annotations.Test;
+
+public class Day2 {
+
+	@Test
+	public void personalLoan() {
+		System.out.println("Good");
+	}
+	
+}
