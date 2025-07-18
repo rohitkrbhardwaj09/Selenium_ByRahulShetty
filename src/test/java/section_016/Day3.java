c532b4f590d02ffba619e05e6ef8edc1be0a627c
Rohit Bhardwaj <rohitkrbhardwaj09@gmail.com>
2025-07-18 13:19:51 +0530

    Day4

---
+++ src/test/java/section_016/Day3.java
@@ -0,0 +1,22 @@
+package section_016;
+
+import org.testng.annotations.Test;
+
+public class Day3 {
+
+	@Test
+	public void WebLoginCarLoan() {
+		System.out.println("WebLoginCar");
+	}
+	
+	@Test
+	public void MobileLoginCarLoan() {
+		System.out.println("MobileLoginCar");
+	}
+	
+	@Test
+	public void APILoginCarLoan() {
+		System.out.println("APILoginCar");
+	}
+	
+}
---
+++ src/test/java/section_016/Day4.java
@@ -0,0 +1,22 @@
+package section_016;
+
+import org.testng.annotations.Test;
+
+public class Day4 {
+
+	@Test
+	public void WebLoginHomeLoan() {
+		System.out.println("WebLoginHome");
+	}
+	
+	@Test
+	public void MobileLoginHomeLoan() {
+		System.out.println("MobileLoginHome");
+	}
+	
+	@Test
+	public void APILoginHomeLoan() {
+		System.out.println("APILoginHome");
+	}
+	
+}
